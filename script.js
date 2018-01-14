ebba611693d38866f08fa3dc07956ed7be534cfd
Soulseek85 <34885427+Soulseek85@users.noreply.github.com>
2018-01-14 19:39:46 +0100

    ok

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var suma = '<img src="img/main.jpg">';
+var i;
+// var i = 2;
 
+// suma = document.getElementById("tresc").innerHTML = '<img src="img/1.png">';
+// document.getElementById("tresc").innerHTML = '<img src="img/1.png">';
+var a = document.getElementById("tresc");
+
+for (i = 1; i <= 12; i++)
+{
+    suma += a.innerHTML = '<img src="img/' + i + '.png">';
+}
+
+a.innerHTML = suma;
