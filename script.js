23079b9f53233c0267bc18bc6f313886b67447c0
Soulseek85 <34885427+Soulseek85@users.noreply.github.com>
2018-01-16 15:24:31 +0100

    ok

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var i;
 
 // suma = document.getElementById("tresc").innerHTML = '<img src="img/1.png">';
 // document.getElementById("tresc").innerHTML = '<img src="img/1.png">';
+
 var a = document.getElementById("tresc");
 
 for (i = 1; i <= 12; i++)
 {
-    suma += a.innerHTML = '<img src="img/' + i + '.png">';
+	suma += a.innerHTML = '<img src="img/' + i + '.png">';
 }
 
 a.innerHTML = suma;
